97d14313468610f3cad2231879c502356c8503cf
Xiao Ling <lingxiao1002@gmail.com>
2021-11-16 17:30:25 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Qt-scan-document-read-barcode
+This is a desktop application developed with Qt Python. It combines [Dynamic Web TWAIN](https://www.dynamsoft.com/web-twain/docs/info/api/?ver=latest) and [Dynamsoft Barcode Reader](https://www.dynamsoft.com/barcode-reader/programming/python/user-guide.html?ver=latest) to implement document scanning and barcode recognition.
+
+## SDK License
+
+**DWT**
+
+[![](https://img.shields.io/badge/Get-30--day%20FREE%20Trial%20License-blue)](https://www.dynamsoft.com/customer/license/trialLicense/?product=dwt)
+
+**DBR**
+
+[![](https://img.shields.io/badge/Get-30--day%20FREE%20Trial%20License-blue)](https://www.dynamsoft.com/customer/license/trialLicense/?product=dbr)
+
+## Install
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+1. Set DWT license in `Resources/dynamsoft.webtwain.config.js`:
+
+    ```js
+    Dynamsoft.DWT.ProductKey = "LICENSE-KEY";
+    ```
+
+2. Set DBR license in `app.py`:
+
+    ```python
+    reader.init_license('LICENSE-KEY')
+    ```
+
+3. Run the application:    
+
+    ```bash
+    python app.py
+    ```  
+
+## Windows Virtual Scanner
+If you don't have a physical scanner, you can follow [https://github.com/yushulx/windows-virtual-scanner](https://github.com/yushulx/windows-virtual-scanner) to build and install a virtual scanner, which allows you to scan custom image set.
---
+++ app.py
@@ -0,0 +1,96 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebChannel import QWebChannel
+
+import os
+from dbr import *
+import base64
+
+# Initialize Dynamsoft Barcode Reader
+reader = BarcodeReader()
+# Apply for a trial license https://www.dynamsoft.com/customer/license/trialLicense?product=dbr
+reader.init_license('LICENSE-KEY')
+
+# Create Qt application
+app = QApplication([])
+win = QWidget()
+win.setWindowTitle('Dynamic Web TWAIN and Dynamsoft Barcode Reader')
+
+# Create a layout
+layout = QVBoxLayout()
+win.setLayout(layout)
+
+class WebView(QWebEngineView):
+    def __init__(self):
+        QWebEngineView.__init__(self)
+
+        # Load web page and resource files to QWebEngineView
+        file_path = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), "index.html"))
+        local_url = QUrl.fromLocalFile(file_path)
+        self.load(local_url)
+
+        self.channel = QWebChannel(self.page())
+        self.channel.registerObject('backend', self)
+        self.page().setWebChannel(self.channel)
+
+    @pyqtSlot(str)
+    def onDataReady(self, base64img):
+        imgdata = base64.b64decode(base64img)
+
+        try:
+            text_results = reader.decode_file_stream(bytearray(imgdata), '')
+            if text_results != None:
+                out = ''
+                for text_result in text_results:
+                    out += "Barcode Format : "
+                    out += text_result.barcode_format_string + '\n'
+                    out += "Barcode Text : "
+                    out += text_result.barcode_text + '\n'
+                    out += "-------------------------------------------------" + '\n'
+
+                text_area.setText(out)
+        except BarcodeReaderError as bre:
+            print(bre)
+
+# Initialize widgets
+view = WebView()
+bt_scan = QPushButton('Scan Barcode Documents')
+bt_read = QPushButton('Read Barcode')
+text_area = QTextEdit()
+
+# Acquire an image
+def acquire_image():
+    frame = view.page()
+    frame.runJavaScript('AcquireImage();')
+
+# Refresh web view
+def refresh_page():
+    view.reload()
+
+# Get base64 image data for barcode reading
+def read_barcode():
+    frame = view.page()
+    frame.runJavaScript('getCurrentImage();')
+
+bt_scan.clicked.connect(acquire_image)
+bt_read.clicked.connect(read_barcode)
+
+layout.addWidget(view)
+layout.addWidget(bt_scan)
+layout.addWidget(bt_read)
+layout.addWidget(text_area)
+
+# Key events
+def keyPressEvent(event):
+    if event.key() == Qt.Key.Key_Q:
+        win.close()
+    elif event.key() == Qt.Key.Key_R:
+        refresh_page()
+
+win.keyPressEvent = keyPressEvent
+
+win.show()
+app.exec_()
